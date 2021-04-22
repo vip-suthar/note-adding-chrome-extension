55a523b42fe5d5ee16d6c566894efef6617e19aa
vipin <vipin29suthar@gmail.com>
2021-04-22 09:23:42 +0530

    some major changes like adding editor

--- dom selector/editor.js
+++ dom selector/editor.js
@@ -1,42 +1,81 @@
-var editor = {
-    html : `<span id="closeEditor">Close</span>
-    <div class="textbox" id="sampleeditor" contenteditable="true"></div>
-        <button id="setDataBtn">Add</button>`
+'use strict';
+
+// Adding stylesheet to page
+const style = document.createElement('link');
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("href", 'https://cdn.quilljs.com/1.3.6/quill.snow.css');
+const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+head.insertBefore(style, head.lastChild);
+
+//Adding Script to page
+const scriptUrl = "https://cdn.quilljs.com/1.3.6/quill.js";
+const loadScript = async (url, callback)=>{
+  const response = await fetch(url);
+  const script = await response.text();
+  (Function(script))();
+  callback();
 }
+
+const addEditor = ()=>{
+  const container = document.getElementById('editor');
+  if (container) {
+    var quill = new Quill(container,{
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block']
+        ]
+      },
+      placeholder: 'Compose an epic...',
+      theme: 'snow'  // or 'bubble' 
+    });
+  }
+}
+
+loadScript(scriptUrl, addEditor);
+
+const editor = {
+  html : `<span id="closeEditor">&times; </span>
+          <div id="editor"></div>
+          <button id="setDataBtn">Add</button>`
+}
+
+const addEditorContainer = (element)=>{
+  const textEditor = document.createElement('div');
+  textEditor.id="editorContainer";
+  textEditor.innerHTML = editor.html;
+  element.classList.add("editorActiveEle");
+  element.after(textEditor);
+  addEditor();
+}
+
 var dataEle = 1;
 document.getElementsByTagName('body')[0].addEventListener('click', function (event) {
-    var targetElement = event.target;
-    var textEditor = document.querySelector("#editor.editor");
-    if (targetElement != document.getElementById('closeEditor')) {
-        if (!textEditor) {
-            textEditor = document.createElement('div');
-            textEditor.id="editor";
-            textEditor.className="editor";
-            textEditor.innerHTML = editor.html;
-            targetElement.parentElement.appendChild(textEditor);
-            var dataId = targetElement.dataId;
-            console.log(dataId);
-            if (dataId) {
-                chrome.storage.local.get([`${dataId}`], function(data) {
-                    console.log(data)
-                document.querySelector('.textbox#sampleeditor').innerHTML = data[`${dataId}`];
-              });
-            }
-            
-
-            document.getElementById('setDataBtn').addEventListener('click', ()=>{
-                var value = document.querySelector("#sampleeditor.textbox").innerHTML;
-                if (!targetElement.dataId) {
-                    targetElement.dataId = `data${dataEle++}`;
-                }
-                var dataId = targetElement.dataId;
-                chrome.storage.local.set({[dataId] : value} , function() {
-                    console.log(`Value is set to  ${value} with dataId ${dataId} : ${value}` );
-                });
-            })
-             
+  const targetElement = event.target;
+  const textEditor = document.querySelector("#editorContainer");
+  var dataId = targetElement.dataId;
+  if (textEditor) {
+    if (targetElement == document.getElementById('closeEditor')) {
+          document.querySelector(".editorActiveEle").classList.remove("editorActiveEle");
+          textEditor.remove();  
+      } 
+  } 
+  else {
+      addEditorContainer(targetElement);
+      if (dataId) {
+        chrome.storage.local.get([`${dataId}`], function(data) {
+        console.log(data)
+        document.querySelector('#editor').innerHTML = data[`${dataId}`];
+      });
+      }
+      document.getElementById('setDataBtn').addEventListener('click', ()=>{
+        var value = document.querySelector("#editor>div.ql-editor").innerHTML;
+        if (!targetElement.dataId) {
+            targetElement.dataId = `data${dataEle++}`;
         }
-    } else {
-        textEditor.remove();
+        dataId = targetElement.dataId;
+            console.log(` ${dataId} : ${value}` );
+      })
     }
 });
